59ad09a2a3fbdcc2d3eec7671df4e7e7212a8e81
vivek kujur <vivekkujur.jnv@gmail.com>
2024-08-08 18:20:20 +0530

    add databinding in onboard fragment and add some buttons for feature testing

--- app/src/main/java/com/example/firebaseloginapp_02/OnboardFragment.kt
+++ app/src/main/java/com/example/firebaseloginapp_02/OnboardFragment.kt
@@ -1,20 +1,47 @@
 package com.example.firebaseloginapp_02
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.firebaseloginapp_02.databinding.ActivityOnboardBinding
+import com.example.firebaseloginapp_02.databinding.FragmentOnboardBinding
+import com.example.firebaseloginapp_02.module.onboard.OnBoardViewModel
 
 
 class OnboardFragment : Fragment() {
+
+    lateinit var  onboardViewModel : OnBoardViewModel
+    var  _binding: FragmentOnboardBinding? = null
+    val binding get() = _binding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboard, container, false)
+
+        _binding = FragmentOnboardBinding.inflate(inflater,container,false)
+
+        onboardViewModel = ViewModelProvider(this).get(OnBoardViewModel::class.java)
+
+        with(onboardViewModel){
+
+            binding?.model = this
+            binding?.lifecycleOwner =  requireActivity()
+
+            onClickLoginLive.observe(requireActivity() , Observer {
+                Toast.makeText(requireActivity(),"button clicked ",Toast.LENGTH_LONG).show();
+            })
+
+        }
+
+        return binding?.root
     }
 
 
--- app/src/main/java/com/example/firebaseloginapp_02/module/onboard/OnBoardViewModel.kt
+++ app/src/main/java/com/example/firebaseloginapp_02/module/onboard/OnBoardViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.firebaseloginapp_02.module.onboard
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class OnBoardViewModel :ViewModel() {
 
+
+    var onClickLoginLive  = MutableLiveData<Unit>()
+    var onclickText = MutableLiveData<String>("login")
+
+    fun loginOnClicked(){
+        onclickText.value = "login button clicked "
+        Log.d("buttonclicked ", "onCreateView: function call ")
+        onClickLoginLive.value = Unit
+
+    }
 }
